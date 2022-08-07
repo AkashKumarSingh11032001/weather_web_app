9d9b972763d23665f0c33e85bf2041bb49b5548a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-08-07 13:09:29 +0530

    search bar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 import Search from './components/search/search';
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(handleOnSearchChange);
+  }
   return (
     <div className="App">
-      <Search />
+      <Search  onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
